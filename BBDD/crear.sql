3c47410101ff83f5a10c1c6671867ebaf9b9d95d
hayk99 <haykk1999@gmail.com>
2019-03-21 09:08:53 +0100

    El codigo de equipos

---
+++ BBDD/crear.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE Equipo (
+	nombreCorto		VARCHAR(20) PRIMARY KEY,
+	nombreOficial		VARCHAR(50),
+	fundacion		NUMBER(4),
+	ciudad			VARCHAR(30)
+);
---
+++ BBDD/equiposCsvToSql.py
@@ -0,0 +1,6 @@
+import fileinput
+
+
+for line in fileinput.input():
+	campos= line.split('\t')
+	print("INSERT INTO Equipo(nombreCorto, nombreOficial, fundacion, ciudad) VALUES ('",campos[0],"','",campos[1],"',",campos[2],",'",campos[3][:len(campos[3])-1], "');", sep='')
